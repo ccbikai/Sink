d06b98d67a727554b225d7063f8c93e1fa8caebf
面条 <2959393+ccbikai@users.noreply.github.com>
2024-05-28 07:52:56 +0800

    docs: typo

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   title: 'Sink',
-  description: 'A Simple / Speedy / Secrue Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
