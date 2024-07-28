b2e859107dfc32d3e0f73da95cd0e0dccc32447e
ccbikai <ccbikai@qq.com>
2024-07-28 13:39:32 +0800

    chore: Update Twitter handle to reflect new username
    Update Twitter URL across multiple files to point to the new username, ensuring consistency and accuracy in social media links.

--- components/home/Twitter.vue
+++ components/home/Twitter.vue
@@ -5,7 +5,7 @@ import { ArrowRight } from 'lucide-vue-next'
 
 <template>
   <a
-    href="https://x.com/xkaibi"
+    href="https://x.com/0xKaiBi"
     target="_blank"
     title="X(Twitter)"
     class="inline-flex items-center px-3 py-1 mx-auto my-4 space-x-1 text-sm font-medium rounded-lg bg-muted"
