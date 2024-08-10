7833f166fc564cf4818715713bb7233697e8f658
Yosafat Kesuma <da005kage@gmail.com>
2024-08-10 14:13:11 +0700

    fix(1.redirect): missing passing query param to url

--- server/middleware/1.redirect.ts
+++ server/middleware/1.redirect.ts
@@ -1,9 +1,10 @@
+import qs from 'querystring'
 import type { z } from 'zod'
 import { parsePath } from 'ufo'
 import type { LinkSchema } from '@/schemas/link'
 
 export default eventHandler(async (event) => {
-  const { pathname: slug } = parsePath(event.path.slice(1)) // remove leading slash
+  const { pathname: slug, search } = parsePath(event.path.slice(1)) // remove leading slash
   const { slugRegex, reserveSlug } = useAppConfig(event)
   const { homeURL } = useRuntimeConfig(event)
   const { cloudflare } = event.context
@@ -14,7 +15,16 @@ export default eventHandler(async (event) => {
   if (slug && !reserveSlug.includes(slug) && slugRegex.test(slug) && cloudflare) {
     const { KV } = cloudflare.env
     const link: z.infer<typeof LinkSchema> | null = await KV.get(`link:${slug}`, { type: 'json' })
+
     if (link) {
+      const _link = link.url.split('?')
+      const newLink = _link[0]
+      let linkQs = { ...qs.parse(search?.replaceAll('?', '')) }
+      if (_link[1]) {
+        linkQs = { ...linkQs, ...qs.parse(`${_link[1]}`) }
+      }
+      link.url = `${newLink}?${qs.stringify(linkQs)}`
+
       event.context.link = link
       try {
         await useAccessLog(event)
