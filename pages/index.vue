a41d6a8f062aef88d966e24c2996530997519e41
fangkampanat <fangkampanat@gmail.com>
2024-09-07 13:10:52 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
-<template>
-  <main class="flex flex-col justify-center">
-    <HomeTwitter />
-    <HomeHero />
-    <HomeFeatures />
-    <HomeLogos />
-    <HomeCta />
-  </main>
-</template>
+<!-- <template>
+    <main class="flex flex-col justify-center">
+      <HomeTwitter />
+      <HomeHero />
+      <HomeFeatures />
+      <HomeLogos />
+      <HomeCta />
+    </main>
+  </template>
+   -->
