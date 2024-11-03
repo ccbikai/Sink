393771631f1b52d4e7056e2c986343cc89111763
ccbikai <ccbikai@qq.com>
2024-11-03 12:52:25 +0800

    chore: update social links to use a unified domain format

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   title: 'Sink',
-  twitter: 'https://x.com/0xKaiBi',
-  github: 'https://github.com/ccbikai/sink',
   email: 'sink.cool@miantiao.me',
-  telegram: 'https://t.me/htmlzone',
-  mastodon: 'https://c.im/@mt',
-  blog: 'https://mt.ci',
+  twitter: 'https://sink.cool/kai',
+  github: 'https://sink.cool/github',
+  telegram: 'https://sink.cool/telegram',
+  mastodon: 'https://sink.cool/mastodon',
+  blog: 'https://sink.cool/blog',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
