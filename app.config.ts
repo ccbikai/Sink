3fdfb42276becdcb1baf66df2f6c22a17f8ccd92
Setasena Randata <setasena@kugie.app>
2024-11-08 23:18:18 +0700

    change site manifest

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
-  title: 'Kug.ie',
+  title: 'Kugie',
   email: 'company@kugie.app',
   blog: 'https://kugie.app/blog',
-  description: 'Kug.ie is Kugie\'s Link Shortener Service',
+  description: 'kugie.dev is Kugie\'s Link Shortener Service',
   image: 'https://kugie.app/og-image.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
