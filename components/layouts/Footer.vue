f595f9aeae2630ccd52be97f66808a83df1d026e
uwaisalqarni <82241016+uwaisalqarni@users.noreply.github.com>
2024-11-25 16:34:20 +0700

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -14,7 +14,7 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
       >{{ title }}</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://html.zone"
+        href="https://indonesianexport.org"
         target="_blank"
         title="HTML.ZONE"
       >
