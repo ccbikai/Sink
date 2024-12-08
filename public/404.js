cf7746fbb1429970e01648917f6e47e285b8aee9
kamilatte <118631823+kamilatte@users.noreply.github.com>
2024-12-08 13:53:45 +0200

    Add files via upload

---
+++ public/404.js
@@ -0,0 +1,720 @@
+/* Carrd Site JS | carrd.co | License: MIT */
+
+(function() {
+
+	// Main.
+		var	on = addEventListener,
+			off = removeEventListener,
+			$ = function(q) { return document.querySelector(q) },
+			$$ = function(q) { return document.querySelectorAll(q) },
+			$body = document.body,
+			$inner = $('.inner'),
+			client = (function() {
+		
+				var o = {
+						browser: 'other',
+						browserVersion: 0,
+						os: 'other',
+						osVersion: 0,
+						mobile: false,
+						canUse: null,
+						flags: {
+							lsdUnits: false,
+						},
+					},
+					ua = navigator.userAgent,
+					a, i;
+		
+				// browser, browserVersion.
+					a = [
+						[
+							'firefox',
+							/Firefox\/([0-9\.]+)/
+						],
+						[
+							'edge',
+							/Edge\/([0-9\.]+)/
+						],
+						[
+							'safari',
+							/Version\/([0-9\.]+).+Safari/
+						],
+						[
+							'chrome',
+							/Chrome\/([0-9\.]+)/
+						],
+						[
+							'chrome',
+							/CriOS\/([0-9\.]+)/
+						],
+						[
+							'ie',
+							/Trident\/.+rv:([0-9]+)/
+						]
+					];
+		
+					for (i=0; i < a.length; i++) {
+		
+						if (ua.match(a[i][1])) {
+		
+							o.browser = a[i][0];
+							o.browserVersion = parseFloat(RegExp.$1);
+		
+							break;
+		
+						}
+		
+					}
+		
+				// os, osVersion.
+					a = [
+						[
+							'ios',
+							/([0-9_]+) like Mac OS X/,
+							function(v) { return v.replace('_', '.').replace('_', ''); }
+						],
+						[
+							'ios',
+							/CPU like Mac OS X/,
+							function(v) { return 0 }
+						],
+						[
+							'ios',
+							/iPad; CPU/,
+							function(v) { return 0 }
+						],
+						[
+							'android',
+							/Android ([0-9\.]+)/,
+							null
+						],
+						[
+							'mac',
+							/Macintosh.+Mac OS X ([0-9_]+)/,
+							function(v) { return v.replace('_', '.').replace('_', ''); }
+						],
+						[
+							'windows',
+							/Windows NT ([0-9\.]+)/,
+							null
+						],
+						[
+							'undefined',
+							/Undefined/,
+							null
+						]
+					];
+		
+					for (i=0; i < a.length; i++) {
+		
+						if (ua.match(a[i][1])) {
+		
+							o.os = a[i][0];
+							o.osVersion = parseFloat( a[i][2] ? (a[i][2])(RegExp.$1) : RegExp.$1 );
+		
+							break;
+		
+						}
+		
+					}
+		
+					// Hack: Detect iPads running iPadOS.
+						if (o.os == 'mac'
+						&&	('ontouchstart' in window)
+						&&	(
+		
+							// 12.9"
+								(screen.width == 1024 && screen.height == 1366)
+							// 10.2"
+								||	(screen.width == 834 && screen.height == 1112)
+							// 9.7"
+								||	(screen.width == 810 && screen.height == 1080)
+							// Legacy
+								||	(screen.width == 768 && screen.height == 1024)
+		
+						))
+							o.os = 'ios';
+		
+				// mobile.
+					o.mobile = (o.os == 'android' || o.os == 'ios');
+		
+				// canUse.
+					var _canUse = document.createElement('div');
+		
+					o.canUse = function(property, value) {
+		
+						var style;
+		
+						// Get style.
+							style = _canUse.style;
+		
+						// Property doesn't exist? Can't use it.
+							if (!(property in style))
+								return false;
+		
+						// Value provided?
+							if (typeof value !== 'undefined') {
+		
+								// Assign value.
+									style[property] = value;
+		
+								// Value is empty? Can't use it.
+									if (style[property] == '')
+										return false;
+		
+							}
+		
+						return true;
+		
+					};
+		
+				// flags.
+					o.flags.lsdUnits = o.canUse('width', '100dvw');
+		
+				return o;
+		
+			}()),
+			ready = {
+				list: [],
+				add: function(f) {
+					this.list.push(f);
+				},
+				run: function() {
+					this.list.forEach((f) => {
+						f();
+					});
+				},
+			},
+			trigger = function(t) {
+				dispatchEvent(new Event(t));
+			},
+			cssRules = function(selectorText) {
+		
+				var ss = document.styleSheets,
+					a = [],
+					f = function(s) {
+		
+						var r = s.cssRules,
+							i;
+		
+						for (i=0; i < r.length; i++) {
+		
+							if (r[i] instanceof CSSMediaRule && matchMedia(r[i].conditionText).matches)
+								(f)(r[i]);
+							else if (r[i] instanceof CSSStyleRule && r[i].selectorText == selectorText)
+								a.push(r[i]);
+		
+						}
+		
+					},
+					x, i;
+		
+				for (i=0; i < ss.length; i++)
+					f(ss[i]);
+		
+				return a;
+		
+			},
+			escapeHtml = function(s) {
+		
+				// Blank, null, or undefined? Return blank string.
+					if (s === ''
+					||	s === null
+					||	s === undefined)
+						return '';
+		
+				// Escape HTML characters.
+					var a = {
+						'&': '&amp;',
+						'<': '&lt;',
+						'>': '&gt;',
+						'"': '&quot;',
+						"'": '&#39;',
+					};
+		
+					s = s.replace(/[&<>"']/g, function(x) {
+						return a[x];
+					});
+		
+				return s;
+		
+			},
+			thisHash = function() {
+		
+				var h = location.hash ? location.hash.substring(1) : null,
+					a;
+		
+				// Null? Bail.
+					if (!h)
+						return null;
+		
+				// Query string? Move before hash.
+					if (h.match(/\?/)) {
+		
+						// Split from hash.
+							a = h.split('?');
+							h = a[0];
+		
+						// Update hash.
+							history.replaceState(undefined, undefined, '#' + h);
+		
+						// Update search.
+							window.location.search = a[1];
+		
+					}
+		
+				// Prefix with "x" if not a letter.
+					if (h.length > 0
+					&&	!h.match(/^[a-zA-Z]/))
+						h = 'x' + h;
+		
+				// Convert to lowercase.
+					if (typeof h == 'string')
+						h = h.toLowerCase();
+		
+				return h;
+		
+			},
+			scrollToElement = function(e, style, duration) {
+		
+				var y, cy, dy,
+					start, easing, offset, f;
+		
+				// Element.
+		
+					// No element? Assume top of page.
+						if (!e)
+							y = 0;
+		
+					// Otherwise ...
+						else {
+		
+							offset = (e.dataset.scrollOffset ? parseInt(e.dataset.scrollOffset) : 0) * parseFloat(getComputedStyle(document.documentElement).fontSize);
+		
+							switch (e.dataset.scrollBehavior ? e.dataset.scrollBehavior : 'default') {
+		
+								case 'default':
+								default:
+		
+									y = e.offsetTop + offset;
+		
+									break;
+		
+								case 'center':
+		
+									if (e.offsetHeight < window.innerHeight)
+										y = e.offsetTop - ((window.innerHeight - e.offsetHeight) / 2) + offset;
+									else
+										y = e.offsetTop - offset;
+		
+									break;
+		
+								case 'previous':
+		
+									if (e.previousElementSibling)
+										y = e.previousElementSibling.offsetTop + e.previousElementSibling.offsetHeight + offset;
+									else
+										y = e.offsetTop + offset;
+		
+									break;
+		
+							}
+		
+						}
+		
+				// Style.
+					if (!style)
+						style = 'smooth';
+		
+				// Duration.
+					if (!duration)
+						duration = 750;
+		
+				// Instant? Just scroll.
+					if (style == 'instant') {
+		
+						window.scrollTo(0, y);
+						return;
+		
+					}
+		
+				// Get start, current Y.
+					start = Date.now();
+					cy = window.scrollY;
+					dy = y - cy;
+		
+				// Set easing.
+					switch (style) {
+		
+						case 'linear':
+							easing = function (t) { return t };
+							break;
+		
+						case 'smooth':
+							easing = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 };
+							break;
+		
+					}
+		
+				// Scroll.
+					f = function() {
+		
+						var t = Date.now() - start;
+		
+						// Hit duration? Scroll to y and finish.
+							if (t >= duration)
+								window.scroll(0, y);
+		
+						// Otherwise ...
+							else {
+		
+								// Scroll.
+									window.scroll(0, cy + (dy * easing(t / duration)));
+		
+								// Repeat.
+									requestAnimationFrame(f);
+		
+							}
+		
+					};
+		
+					f();
+		
+			},
+			scrollToTop = function() {
+		
+				// Scroll to top.
+					scrollToElement(null);
+		
+			},
+			loadElements = function(parent) {
+		
+				var a, e, x, i;
+		
+				// IFRAMEs.
+		
+					// Get list of unloaded IFRAMEs.
+						a = parent.querySelectorAll('iframe[data-src]:not([data-src=""])');
+		
+					// Step through list.
+						for (i=0; i < a.length; i++) {
+		
+							// Load.
+								a[i].contentWindow.location.replace(a[i].dataset.src);
+		
+							// Save initial src.
+								a[i].dataset.initialSrc = a[i].dataset.src;
+		
+							// Mark as loaded.
+								a[i].dataset.src = '';
+		
+						}
+		
+				// Video.
+		
+					// Get list of videos (autoplay).
+						a = parent.querySelectorAll('video[autoplay]');
+		
+					// Step through list.
+						for (i=0; i < a.length; i++) {
+		
+							// Play if paused.
+								if (a[i].paused)
+									a[i].play();
+		
+						}
+		
+				// Autofocus.
+		
+					// Get first element with data-autofocus attribute.
+						e = parent.querySelector('[data-autofocus="1"]');
+		
+					// Determine type.
+						x = e ? e.tagName : null;
+		
+						switch (x) {
+		
+							case 'FORM':
+		
+								// Get first input.
+									e = e.querySelector('.field input, .field select, .field textarea');
+		
+								// Found? Focus.
+									if (e)
+										e.focus();
+		
+								break;
+		
+							default:
+								break;
+		
+						}
+		
+				// Embeds.
+		
+					// Get unloaded embeds.
+						a = parent.querySelectorAll('unloaded-script');
+		
+					// Step through list.
+						for (i=0; i < a.length; i++) {
+		
+							// Create replacement script tag.
+								x = document.createElement('script');
+		
+							// Set "loaded" data attribute (so we can unload this element later).
+								x.setAttribute('data-loaded', '');
+		
+							// Set "src" attribute (if present).
+								if (a[i].getAttribute('src'))
+									x.setAttribute('src', a[i].getAttribute('src'));
+		
+							// Set text content (if present).
+								if (a[i].textContent)
+									x.textContent = a[i].textContent;
+		
+							// Replace.
+								a[i].replaceWith(x);
+		
+						}
+		
+				// Everything else.
+		
+					// Create "loadelements" event.
+						x = new Event('loadelements');
+		
+					// Get unloaded elements.
+						a = parent.querySelectorAll('[data-unloaded]');
+		
+					// Step through list.
+						a.forEach((element) => {
+		
+							// Clear attribute.
+								element.removeAttribute('data-unloaded');
+		
+							// Dispatch event.
+								element.dispatchEvent(x);
+		
+						});
+		
+			},
+			unloadElements = function(parent) {
+		
+				var a, e, x, i;
+		
+				// IFRAMEs.
+		
+					// Get list of loaded IFRAMEs.
+						a = parent.querySelectorAll('iframe[data-src=""]');
+		
+					// Step through list.
+						for (i=0; i < a.length; i++) {
+		
+							// Don't unload? Skip.
+								if (a[i].dataset.srcUnload === '0')
+									continue;
+		
+							// Mark as unloaded.
+		
+								// IFRAME was previously loaded by loadElements()? Use initialSrc.
+									if ('initialSrc' in a[i].dataset)
+										a[i].dataset.src = a[i].dataset.initialSrc;
+		
+								// Otherwise, just use src.
+									else
+										a[i].dataset.src = a[i].src;
+		
+							// Unload.
+								a[i].contentWindow.location.replace('about:blank');
+		
+						}
+		
+				// Video.
+		
+					// Get list of videos.
+						a = parent.querySelectorAll('video');
+		
+					// Step through list.
+						for (i=0; i < a.length; i++) {
+		
+							// Pause if playing.
+								if (!a[i].paused)
+									a[i].pause();
+		
+						}
+		
+				// Autofocus.
+		
+					// Get focused element.
+						e = $(':focus');
+		
+					// Found? Blur.
+						if (e)
+							e.blur();
+		
+				// Embeds.
+				// NOTE: Disabled for now. May want to bring this back later.
+				/*
+		
+					// Get loaded embeds.
+						a = parent.querySelectorAll('script[data-loaded]');
+		
+					// Step through list.
+						for (i=0; i < a.length; i++) {
+		
+							// Create replacement unloaded-script tag.
+								x = document.createElement('unloaded-script');
+		
+							// Set "src" attribute (if present).
+								if (a[i].getAttribute('src'))
+									x.setAttribute('src', a[i].getAttribute('src'));
+		
+							// Set text content (if present).
+								if (a[i].textContent)
+									x.textContent = a[i].textContent;
+		
+							// Replace.
+								a[i].replaceWith(x);
+		
+						}
+		
+				*/
+		
+			};
+		
+			// Expose scrollToElement.
+				window._scrollToTop = scrollToTop;
+	
+	// Load elements.
+		// Load elements (if needed).
+			loadElements(document.body);
+	
+	// Browser hacks.
+		// Init.
+			var style, sheet, rule;
+		
+			// Create <style> element.
+				style = document.createElement('style');
+				style.appendChild(document.createTextNode(''));
+				document.head.appendChild(style);
+		
+			// Get sheet.
+				sheet = style.sheet;
+		
+		// Mobile.
+			if (client.mobile) {
+		
+				// Prevent overscrolling on Safari/other mobile browsers.
+				// 'vh' units don't factor in the heights of various browser UI elements so our page ends up being
+				// a lot taller than it needs to be (resulting in overscroll and issues with vertical centering).
+					(function() {
+		
+						// Lsd units available?
+							if (client.flags.lsdUnits) {
+		
+								document.documentElement.style.setProperty('--viewport-height', '100svh');
+								document.documentElement.style.setProperty('--background-height', '100lvh');
+		
+							}
+		
+						// Otherwise, use innerHeight hack.
+							else {
+		
+								var f = function() {
+									document.documentElement.style.setProperty('--viewport-height', window.innerHeight + 'px');
+									document.documentElement.style.setProperty('--background-height', (window.innerHeight + 250) + 'px');
+								};
+		
+								on('load', f);
+								on('orientationchange', function() {
+		
+									// Update after brief delay.
+										setTimeout(function() {
+											(f)();
+										}, 100);
+		
+								});
+		
+							}
+		
+					})();
+		
+			}
+		
+		// Android.
+			if (client.os == 'android') {
+		
+				// Prevent background "jump" when address bar shrinks.
+				// Specifically, this fix forces the background pseudoelement to a fixed height based on the physical
+				// screen size instead of relying on "vh" (which is subject to change when the scrollbar shrinks/grows).
+					(function() {
+		
+						// Insert and get rule.
+							sheet.insertRule('body::after { }', 0);
+							rule = sheet.cssRules[0];
+		
+						// Event.
+							var f = function() {
+								rule.style.cssText = 'height: ' + (Math.max(screen.width, screen.height)) + 'px';
+							};
+		
+							on('load', f);
+							on('orientationchange', f);
+							on('touchmove', f);
+		
+					})();
+		
+				// Apply "is-touch" class to body.
+					$body.classList.add('is-touch');
+		
+			}
+		
+		// iOS.
+			else if (client.os == 'ios') {
+		
+				// <=11: Prevent white bar below background when address bar shrinks.
+				// For some reason, simply forcing GPU acceleration on the background pseudoelement fixes this.
+					if (client.osVersion <= 11)
+						(function() {
+		
+							// Insert and get rule.
+								sheet.insertRule('body::after { }', 0);
+								rule = sheet.cssRules[0];
+		
+							// Set rule.
+								rule.style.cssText = '-webkit-transform: scale(1.0)';
+		
+						})();
+		
+				// <=11: Prevent white bar below background when form inputs are focused.
+				// Fixed-position elements seem to lose their fixed-ness when this happens, which is a problem
+				// because our backgrounds fall into this category.
+					if (client.osVersion <= 11)
+						(function() {
+		
+							// Insert and get rule.
+								sheet.insertRule('body.ios-focus-fix::before { }', 0);
+								rule = sheet.cssRules[0];
+		
+							// Set rule.
+								rule.style.cssText = 'height: calc(100% + 60px)';
+		
+							// Add event listeners.
+								on('focus', function(event) {
+									$body.classList.add('ios-focus-fix');
+								}, true);
+		
+								on('blur', function(event) {
+									$body.classList.remove('ios-focus-fix');
+								}, true);
+		
+						})();
+		
+				// Apply "is-touch" class to body.
+					$body.classList.add('is-touch');
+		
+			}
+	
+	// Run ready handlers.
+		ready.run();
+
+})();
