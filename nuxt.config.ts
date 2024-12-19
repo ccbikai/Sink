a03043a40adef1100983c3ff7be85ed278db65b9
uwaisalqarni <82241016+uwaisalqarni@users.noreply.github.com>
2024-12-19 11:51:58 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,6 +60,7 @@ export default defineNuxtConfig({
     dataset: 'sink',
     aiModel: '@cf/meta/llama-3.1-8b-instruct',
     aiPrompt: `You are a URL shortening assistant, please shorten the URL provided by the user into a SLUG. The SLUG information must come from the URL itself, do not make any assumptions. A SLUG is human-readable and should not exceed three words and can be validated using regular expressions {slugRegex} . Only the best one is returned, the format must be JSON reference {"slug": "example-slug"}`,
+    caseSensitive: false,
     public: {
       previewMode: '',
       slugDefaultLength: '6',
