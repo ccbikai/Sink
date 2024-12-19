3dd74504351c9e173e2089203f0757197e021ac5
uwaisalqarni <82241016+uwaisalqarni@users.noreply.github.com>
2024-12-19 11:57:36 +0700

    Add files via upload

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -14,12 +14,90 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
       >{{ title }}</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://indonesianexport.org"
+        href="https://html.zone"
         target="_blank"
         title="HTML.ZONE"
       >
-        &copy; {{ new Date().getFullYear() }} Indonesian Export Channel
+        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
       </a>
+      <span
+        class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
+      >
+        <a
+          v-if="email"
+          :href="`mailto:${email}`"
+          title="Email"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Email</span>
+          <GmailIcon
+            class="w-6 h-6"
+          />
+        </a>
+        <a
+          v-if="telegram"
+          :href="telegram"
+          target="_blank"
+          title="Telegram"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Telegram</span>
+          <TelegramIcon
+            class="w-6 h-6"
+          />
+        </a>
+        <a
+          v-if="blog"
+          :href="blog"
+          target="_blank"
+          title="Blog"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Blog</span>
+          <BloggerIcon
+            class="w-6 h-6"
+          />
+        </a>
+
+        <a
+          v-if="twitter"
+          :href="twitter"
+          target="_blank"
+          title="Twitter"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Twitter</span>
+          <XIcon
+            class="w-6 h-6"
+          />
+        </a>
+
+        <a
+          v-if="mastodon"
+          :href="mastodon"
+          target="_blank"
+          title="Mastodon"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Mastodon</span>
+          <MastodonIcon
+            class="w-6 h-6"
+          />
+        </a>
+
+        <a
+          v-if="github"
+          :href="github"
+          target="_blank"
+          title="GitHub"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">GitHub</span>
+          <GitHubIcon
+            class="w-6 h-6"
+          />
+        </a>
+      </span>
     </div>
   </section>
 </template>
