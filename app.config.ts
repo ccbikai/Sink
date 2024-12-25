8e620022e89224867104246f7b37177be5d4c9e0
Yin Li Qiang <1445482881@qq.com>
2024-12-25 16:49:20 +0800

    Update app.config.ts
    update contact

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,7 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  title: 'Slink',
+  telegram: 'https://t.me/AccessDeny_bot',
+  description: '本页面为引导页面，请联系tgbot发送您需要缩短的链接。',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
