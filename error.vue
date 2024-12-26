34c6b8200202d6308e6ea7158032b5eb70bd5289
mnkngdl <manekingidol@gmail.com>
2024-12-26 23:08:36 +0700

    Update error.vue

--- error.vue
+++ error.vue
@@ -3,11 +3,9 @@ import errorImage from './assets/images/404.svg?raw'
 </script>
 
 <template>
-  //<NuxtLayout name="default">
     <a
       class="w-full h-full max-w-[600px] mx-auto flex items-center justify-center [&_svg]:w-full"
       href="/"
       v-html="errorImage"
     />
-  //</NuxtLayout>
 </template>
