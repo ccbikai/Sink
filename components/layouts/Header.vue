d62817cf3ae9d9affde66f3d5c352c8d0d0b3408
didingqd <wy_bjj@126.com>
2024-12-28 20:40:48 +0800

    Update Header.vue
    汉化

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -64,7 +64,7 @@ const { title, github } = useAppConfig()
                 class="w-full px-6 py-2 mr-0 text-gray-700 cursor-pointer dark:text-gray-300 md:px-3 md:mr-2 lg:mr-3 md:w-auto"
                 href="/dashboard"
                 :title="`${title} Dashboard`"
-              >Dashboard</a>
+              >仪表板</a>
               <a
                 :href="github"
                 target="_blank"
