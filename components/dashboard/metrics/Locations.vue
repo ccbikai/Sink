888aa7c68b3a9740375c2fc2724685eb83c6e097
didingqd <wy_bjj@126.com>
2024-12-28 21:55:00 +0800

    Update Locations.vue
    汉化

--- components/dashboard/metrics/Locations.vue
+++ components/dashboard/metrics/Locations.vue
@@ -61,7 +61,7 @@ const Tooltip = {
 <template>
   <Card class="flex flex-col md:h-[500px]">
     <CardHeader>
-      <CardTitle>Locations</CardTitle>
+      <CardTitle>地点</CardTitle>
     </CardHeader>
     <CardContent class="flex-1 flex [&_[data-radix-aspect-ratio-wrapper]]:flex-1">
       <AspectRatio :ratio="65 / 30">
