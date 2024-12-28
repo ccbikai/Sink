84fce814f3cb17b9d884406849238b123222965c
didingqd <wy_bjj@126.com>
2024-12-28 21:28:44 +0800

    Update Twitter.vue
    汉化修复错误

--- components/home/Twitter.vue
+++ components/home/Twitter.vue
@@ -2,12 +2,12 @@
 import { GitHubIcon } from 'vue3-simple-icons'
 import { ArrowRight } from 'lucide-vue-next'
 
-const { gitHub } = useAppConfig()
+const { github } = useAppConfig()
 </script>
 
 <template>
   <a
-    :href="gitHub"
+    :href="github"
     target="_blank"
     title="GitHub"
     class="inline-flex items-center px-3 py-1 mx-auto my-4 space-x-1 text-sm font-medium rounded-lg bg-muted"
