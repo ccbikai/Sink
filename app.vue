4b956e47dab90c792e91ccbded359c8528b6cf58
fangkampanat <fangkampanat@gmail.com>
2025-05-07 12:52:53 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -23,6 +23,7 @@ useHead({
       content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
       tagPosition: 'head',
     },
+    { name: 'robots', content: 'noindex, nofollow', tagPosition: 'head', }
   ],
   link: [
     {
