72158b4125d6034a97e50491263bddb3d51b8b6b
fangkampanat <fangkampanat@gmail.com>
2025-05-07 13:35:48 +0700

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
-  title: 'Sink',
+  title: 'Sinks',
   email: 'sink.cool@miantiao.me',
   github: 'https://github.com/ccbikai/sink',
   twitter: 'https://sink.cool/kai',
   telegram: 'https://sink.cool/telegram',
   mastodon: 'https://sink.cool/mastodon',
   blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: '',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
