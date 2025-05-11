66fa4e66502307a1dff49157c4e359e5c7b30bb2
ccbikai <ccbikai@gmail.com>
2025-05-11 13:16:45 +0800

    fix: update footer link to point to the new site
    - Changes the footer logo link from the root path to the new URL "https://sink.cool" for better accessibility and branding.

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -8,7 +8,7 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
   <section class="md:pt-6">
     <div class="container flex flex-col items-center py-8 mx-auto sm:flex-row">
       <a
-        href="/"
+        href="https://sink.cool"
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
         :title="title"
       >{{ title }}</a>
