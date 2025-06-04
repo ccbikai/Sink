899a1d67effdbdd5b0d365b961df1564c2c2b5f8
Green <jason@green.cx>
2025-06-03 22:32:17 -0500

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,13 +1,13 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  title: 'Flyto.My',
+  email: 'noreply@gmail.com',
+  github: 'https://flyto.my/github',
+  twitter: 'https://flyto.my/twitter',
+  telegram: 'https://flyto.my/telegram',
+  mastodon: 'https://flyto.my/mastodon',
+  blog: 'https://flyto.my/blog',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://flyto.my/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
