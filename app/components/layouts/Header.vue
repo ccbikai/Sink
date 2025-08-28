beddabd15ddf2daba5ff824d36c5fa39a5067ba8
Suyu <syuim.im@gmail.com>
2025-08-28 11:05:25 +0800

    Update Header.vue

--- app/components/layouts/Header.vue
+++ app/components/layouts/Header.vue
@@ -70,10 +70,7 @@ const { title, github } = useAppConfig()
                 title="Github"
                 class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:px-3 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800 mr-2"
               >
-                <GitHubIcon
-                  class="w-5 h-5 mr-1"
-                />
-                GitHub</a>
+                
 
               <SwitchLanguage />
 
