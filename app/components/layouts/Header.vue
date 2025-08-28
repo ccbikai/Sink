68152404516ea127fcd2cd6af0271a90e93d4e7f
Suyu <syuim.im@gmail.com>
2025-08-28 11:10:01 +0800

    Update Header.vue

--- app/components/layouts/Header.vue
+++ app/components/layouts/Header.vue
@@ -64,12 +64,7 @@ const { title, github } = useAppConfig()
                 href="/dashboard"
                 :title="`${title} Dashboard`"
               >{{ $t('dashboard.title') }}</a>
-              <a
-                :href="github"
-                target="_blank"
-                title="Github"
-                class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:px-3 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800 mr-2"
-              >
+            
                 
 
               <SwitchLanguage />
